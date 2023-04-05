ecf3239a4ae150abca6870d1a2119a6cb576a74c
com12 <mitsuha1@outlook.com>
2023-04-05 09:32:14 +0300

    zd1

--- z1.cpp
+++ z1.cpp
@@ -25,7 +25,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     // Create the window.
 
     HWND hwnd = CreateWindowEx(
-            0,                              // Optional window styles.
+            1,                              // Optional window styles.
             CLASS_NAME,                     // Window class
             L"Learn to Program Windows",    // Window text
             WS_OVERLAPPEDWINDOW,            // Window style
@@ -108,22 +108,30 @@ void pkgInfo(HWND hwnd)
 }
 void setText(HWND hwnd, int width, int height, RECT rect) {
     PAINTSTRUCT ps;
+
     HDC hdc = BeginPaint(hwnd, &ps);
+
     HDC memdc = CreateCompatibleDC(hdc);
+
     HBITMAP membitmap = CreateCompatibleBitmap(hdc, width, height);
     SelectObject(memdc, membitmap);
-
+    HBRUSH hBrush = CreateSolidBrush(RGB(5, 4, 82));
+    auto hOldBrush = (HBRUSH)SelectObject(memdc, hBrush);
+    FillRect(memdc, &rect, hBrush);
+    SelectObject(memdc, hOldBrush);
     // 在内存缓冲区中绘制内容
     HFONT hFont = CreateFont(20, 0, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE,
                              DEFAULT_CHARSET, OUT_OUTLINE_PRECIS, CLIP_DEFAULT_PRECIS, CLEARTYPE_QUALITY,
                              VARIABLE_PITCH, TEXT("Arial"));
     SelectObject(memdc, hFont);
-    SetTextColor(memdc, RGB(0, 0, 255));
+    SetTextColor(memdc, RGB(255, 255, 255));
+    //SetBkMode(memdc, TRANSPARENT);
     SetBkMode(memdc, TRANSPARENT);
-    DrawText(memdc, TEXT("居中文本"), -1, &rect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
-
+    DrawText(memdc, TEXT("System Error! Code:0x000000C"), -1, &rect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+    //FillRect(hdc, &ps.rcPaint, hBrush);
     // 将内存缓冲区中的内容绘制到屏幕上
     BitBlt(hdc, 0, 0, width, height, memdc, 0, 0, SRCCOPY);
+
     // 释放资源
     DeleteObject(hFont);
     DeleteObject(membitmap);
